7e5fa3a19000ebb51c8bddfc00e8a122f817ea07
kimminwook <coreminw@naver.com>
2022-10-29 17:03:52 +0900

    [CHORE] res message 수정

--- seminar3/src/controllers/ContentController.ts
+++ seminar3/src/controllers/ContentController.ts
@@ -3,14 +3,21 @@ import ContentService from "../services/ContentService";
 
 const getContent = async (req: Request, res: Response) => {
   const { contentId } = req.params;
+  const episodeId = req.query.episodeId as string;
 
   const data = await ContentService.getContent(contentId);
+  const episodeIdData = await ContentService.getEpisode(episodeId);
+  console.log(episodeId);
+  
   
   if(!data){
-    return res.status(404).json({status: 404 ,message:"찾을 수 없음"});
+    return res.status(404).json({status: 404 ,message:`${data.title}을 찾을 수 없음`});
+  }
+  if(!episodeIdData){
+    return res.status(404).json({status: 404 ,message:`${data.title}을 찾을 수 없음`});
   }
 
-  return res.status(200).json({status: 200 ,message:"수리남 조회 성공", data});
+  return res.status(200).json({status: 200 ,message: `${data.title}`, data});
 };
 
 export default {
