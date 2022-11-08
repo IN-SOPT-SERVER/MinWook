ed72ea91f5c877083fecf95d646c28357ad976b5
kimminwook <coreminw@naver.com>
2022-11-08 22:52:57 +0900

    [ADD] seminar4 실천 과제

--- seminar4/src/controller/userController.ts
+++ seminar4/src/controller/userController.ts
@@ -17,24 +17,50 @@ const createUser = async (req: Request, res: Response) => {
   const { userName, email, age } = req.body;
   const { userId } = req.params;
 
+  if (!userName || !email || !age) {
+    return res.status(400).json({ status: 404, message: "유저 생성 실패" });
+  }
+
   const data = await userService.createUser(userName, email, age);
 
-  if(!data){
+  if (!data) {
     return res.status(400).json({ status: 404, message: "유저 생성 실패" });
   }
 
   return res.status(200).json({ status: 200, message: "유저 생성 성공", data });
-
 };
 
 //유저 전체 조회
-const getAllUser = async (req: Request, res: Response) => {};
+const getAllUser = async (req: Request, res: Response) => {
+  const data = await userService.getAllUser();
+  return res
+    .status(200)
+    .json({ status: 200, message: "유저 전체 조회 성공", data });
+};
 
 //유저 정보 업데이트
-const updateUser = async (req: Request, res: Response) => {};
+const updateUser = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const { userId } = req.params;
+
+  if (!name) {
+    return res.status(400).json({ status: 404, message: "유저 업데이트 실패" });
+  }
+
+  const updateUser = await userService.updateUser(+userId, name);
+
+  return res
+    .status(200)
+    .json({ status: 200, message: "유저 업데이트 성공", updateUser });
+};
 
 // 유저 삭제
-const deleteUser = async (req: Request, res: Response) => {};
+const deleteUser = async (req: Request, res: Response) => {
+  const {userId} = req.params;
+
+  await userService.deleteUser(+userId);
+
+};
 
 const userController = {
   getUserById,
